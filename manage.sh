56c973c1cdf90bd59dcf65ed4016372deb94ee24
Rotoro.cloud <72907533+rotoro-cloud@users.noreply.github.com>
2024-12-13 20:56:24 +0300

    Update manage.sh

--- manage.sh
+++ manage.sh
@@ -1,4 +1,9 @@
 #/bin/bash
+[ ! -f file-1.dat ] && dd if=/dev/zero of=file-1.dat bs=7500 count=1
+[ ! -f file-2.dat ] && dd if=/dev/zero of=file-2.dat bs=13500 count=1
+[ ! -f file-3.dat ] && dd if=/dev/zero of=file-3.dat bs=19000 count=1
+[ ! -f file-4.dat ] && dd if=/dev/zero of=file-4.dat bs=21000 count=1
+
 now=$(date +%s)
 end=$((now + 7200))
 
