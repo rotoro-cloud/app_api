28c4dcbb8e82b105119eae58170eaaeae9dd4e1d
Rotoro.cloud <72907533+rotoro-cloud@users.noreply.github.com>
2024-12-13 20:36:51 +0300

    Update manage.sh

--- manage.sh
+++ manage.sh
@@ -1,19 +1,16 @@
 #/bin/bash
-
 now=$(date +%s)
 end=$((now + 7200))
 
 while [ $((end - now)) -gt 0 ]; do
-  echo no
-  sleep 10
-  now=$(date +%s)
-  echo $now
-
+	echo no
+	sleep 10
+	now=$(date +%s)
+	echo $now
   api=$(( ( RANDOM %3 ) + 1 ))
-  if [ "$api" -eq 1 ]; then
-    ./req.sh
-  else
-    sleep 5
-  fi
-
+    if [ "$api" -eq 1 ]; then
+      ./req.sh
+    else
+	    sleep 5
+    fi
 done
